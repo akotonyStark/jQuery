de6fc5894d34c76bc7b2009e06589ec246595474
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-04-25 19:10:46 +0000

    Update 2. selectors.js

--- 2. selectors.js
+++ 2. selectors.js
@@ -4,7 +4,10 @@ $(document).ready(() => {
     $("div").css('background-color', 'orange')
     
     $('.para').css('font-style', 'italic')
+    $('#div1, #div2').css('background-color', 'red')
+     $('#div3 li').css('background-color', 'purple')
   })
+  
   
  
 })
