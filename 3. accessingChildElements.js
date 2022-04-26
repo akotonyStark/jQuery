9facb7a1c6d438d322c140dafb65d4e8c2a3e703
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-04-26 16:44:14 +0000

    Update 3. accessingChildElements.js

--- 3. accessingChildElements.js
+++ 3. accessingChildElements.js
@@ -9,4 +9,9 @@ $(document).ready(() => {
   $("li[name]").append(" and juice")
  
    $("li[id='apple']").append(" and juice")
+  $("a[href *= 'google']").css("font-weight", "bold")
+  
+  $("a[href ^= 'www']").css({"font-weight": "bold", "color": "darkred"})
+  
+  $("img").width(500).height(500)
 })
