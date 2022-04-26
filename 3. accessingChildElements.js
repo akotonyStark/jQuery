ff6c9fa682dc70dad9c3dc4ae5bf1e913eda2264
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-04-26 09:45:38 +0000

    Create 3. accessingChildElements.js

---
+++ 3. accessingChildElements.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {  
+
+    $('#wrapper').css("width", 500)
+    $('em > a').css("color", "red")
+   $('em + a').css("color", "orange")
+  $("#fruitsList li:nth-child(3)").css("color", "red")
+ 
+})
