17864b41267207383312d14a10872d478c383e94
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-04-26 09:51:24 +0000

    Update 3. accessingChildElements.js

--- 3. accessingChildElements.js
+++ 3. accessingChildElements.js
@@ -4,5 +4,8 @@ $(document).ready(() => {
     $('em > a').css("color", "red")
    $('em + a').css("color", "orange")
   $("#fruitsList li:nth-child(3)").css("color", "red")
+  
+  $("#fruitsList li:first").css("color", "blue")
+  $("li[name]").append(" and juice")
  
 })
